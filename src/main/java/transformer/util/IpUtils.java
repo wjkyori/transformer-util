f94941dc5fd5cb559e758262eee8f09dfef80a09
wangjk <49204399@qq.com>
2017-02-21 15:15:45 +0800

    google style checked

--- src/main/java/transformer/util/IpUtils.java
+++ src/main/java/transformer/util/IpUtils.java
@@ -14,37 +14,37 @@ import javax.servlet.http.HttpServletRequest;
  */
 public final class IpUtils {
 
-	/**
-	 * Instantiates a new ip utils.
-	 */
-	private IpUtils() {
-	}
+  /**
+   * Instantiates a new ip utils.
+   */
+  private IpUtils() {
+  }
 
-	/**
-	 * 获取请求真实IP地址.
-	 * @param request 请求体
-	 * @return IP地址
-	 */
-	public static String getIpAddr(HttpServletRequest request) {
-		if (request == null) {
-			return "unknown";
-		}
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("X-Forwarded-For");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("X-Real-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		return ip;
-	}
+  /**
+   * 获取请求真实IP地址.
+   * @param request 请求体
+   * @return IP地址
+   */
+  public static String getIpAddr(HttpServletRequest request) {
+    if (request == null) {
+      return "unknown";
+    }
+    String ip = request.getHeader("x-forwarded-for");
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("X-Forwarded-For");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("WL-Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("X-Real-IP");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getRemoteAddr();
+    }
+    return ip;
+  }
 }
--- src/main/java/transformer/util/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright (c) 2005-2017 transformer All rights reserved.
- * @author wjk
- */
-package transformer.util;
